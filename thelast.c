158caf2144427cad01ff4cde5d9c8c07ad75dcf7
José Maia da Silva Neto <maianeto2014@hotmail.com>
2022-12-21 11:21:21 -0300

    Projeto UNO FINAL 21/12/2022 08:21

--- thelast.c
+++ thelast.c
@@ -4,7 +4,7 @@
 // RESOLVER PROBLEMA DO TABLE
 // ./uno bot_A bot_X -s 65708 ou 65970
 // gcc main.c -o bot_X
-// VERSÃO DE TESTES
+// VERSÃO ESTÁVEL PRONTA PARA ENTREGA
 
 #define MAX_LINE 100
 #define MAX_ACTION 10
@@ -35,12 +35,6 @@ enum naipes {
   naipe_ouros = 4 
 };
 
-typedef struct {
-  char carta[10];
-  char valor[10];
-  char naipe[10];
-} Card;
-
 // Função para verificar tamanho da string e tratá-la, separando o valor e o naipe
 void trataCarta(char* carta, char* valor, char* naipe){
   if(strlen(carta) == 4){
@@ -52,21 +46,21 @@ void trataCarta(char* carta, char* valor, char* naipe){
 }
 
 // FUNÇÃO PARA COMPRAR CARTAS
-void buy(int qte, Card* cartas, int* hand_size){
+void buy(int qte, char cartas[][100], int* hand_size){
   printf("BUY %d\n", qte);
   *hand_size += qte;
   if(qte == 4){
-    scanf("%s %s %s %s", cartas[(*hand_size)-1].carta, cartas[(*hand_size)-2].carta, cartas[(*hand_size)-3].carta, cartas[(*hand_size)-4].carta);
+    scanf("%s %s %s %s", cartas[(*hand_size)-1], cartas[(*hand_size)-2], cartas[(*hand_size)-3], cartas[(*hand_size)-4]);
   }
   else if(qte == 2){
-    scanf("%s %s", cartas[(*hand_size)-1].carta, cartas[(*hand_size)-2].carta);
+    scanf("%s %s", cartas[(*hand_size)-1], cartas[(*hand_size)-2]);
   }
   else{
-    scanf("%s", cartas[(*hand_size)-1].carta);
+    scanf("%s", cartas[(*hand_size)-1]);
   }
 }
 
-// FUNÇÃO PARA ENCONTRAR O MAIOR ENTRE DOIS INTEIROS
+// MAIOR
 int max(int a, int b){
   if(a > b){
     return a;
@@ -77,16 +71,16 @@ int max(int a, int b){
 }
 
 // CALCULA QUANTAS CARTAS DE CADA NAIPE O BOT POSSUI 
-int trocarNaipe(Card* cartas, int hand_size, char* valor, char* naipe){
+int trocarNaipe(char cartas[][100], int hand_size, char* valor, char* naipe){
   int qte_copas, qte_ouros, qte_espadas, qte_paus;
   int maior;
   qte_copas = qte_ouros = qte_espadas = qte_paus = 0;
   for(int i = 0; i < hand_size; i++){
-    if(strlen(cartas[i].carta) == 4){
-      sscanf(cartas[i].carta, "%1s %4s", valor, naipe); 
+    if(strlen(cartas[i]) == 4){
+      sscanf(cartas[i], "%1s %4s", valor, naipe); 
     }
     else{
-      sscanf(cartas[i].carta, "%2s %4s", valor, naipe); 
+      sscanf(cartas[i], "%2s %4s", valor, naipe); 
     }
     if(strcmp(naipe, espadas) == 0){
       qte_espadas++;
@@ -124,8 +118,7 @@ int main() {
   int hand_size = 7;
   boolean encontrado;
 
-  //char cartas[100][100];
-  Card cartas[100];
+  char cartas[100][100];
   char valor[20], naipe[20];
   char carta_valor[20], carta_naipe[20];
   
@@ -141,8 +134,8 @@ int main() {
   scanf("HAND [ %[^\n]\n", temp); // Recebe as cartas inicias
   // Faz o tratamento e armazenamento das cartas
   strncpy(my_hand, temp, strlen(temp) - 1);
-  sscanf(my_hand, "%s %s %s %s %s %s %s", cartas[0].carta, cartas[1].carta, cartas[2].carta,
-         cartas[3].carta, cartas[4].carta, cartas[5].carta, cartas[6].carta);
+  sscanf(my_hand, "%s %s %s %s %s %s %s", cartas[0], cartas[1], cartas[2],
+         cartas[3], cartas[4], cartas[5], cartas[6]);
 
   // Lê a carta aberta sobre a mesa.
   scanf("TABLE %s\n", table);
@@ -179,36 +172,36 @@ int main() {
     else{
       encontrado = true;
       for(int i = 0; i < hand_size; i++){
-        trataCarta(cartas[i].carta, cartas[i].valor, cartas[i].naipe);
-        if((strcmp(cartas[i].valor, valor) == 0) || (strcmp(cartas[i].naipe, naipe) == 0)){
-          if((strcmp(cartas[i].valor, "A") == 0) || strcmp(cartas[i].valor, "C") == 0 ){
-            strcpy(table, cartas[i].valor);
+        trataCarta(cartas[i], carta_valor, carta_naipe);
+        if((strcmp(carta_valor, valor) == 0) || (strcmp(carta_naipe, naipe) == 0)){
+          if((strcmp(carta_valor, "A") == 0) || strcmp(carta_valor, "C") == 0 ){
+            strcpy(table, carta_valor);
 
             // Estratégia para escolher naipe
-            if(trocarNaipe(cartas, hand_size, cartas[i].valor, cartas[i].naipe) == naipe_espadas){
+            if(trocarNaipe(cartas, hand_size, carta_valor, carta_naipe) == naipe_espadas){
               strcat(table, espadas);
               printf("DISCARD %s %s\n", cartas[i], espadas);
             }
-            else if(trocarNaipe(cartas, hand_size, cartas[i].valor, cartas[i].naipe) == naipe_paus){
+            else if(trocarNaipe(cartas, hand_size, carta_valor, carta_naipe) == naipe_paus){
               strcat(table, paus);
               printf("DISCARD %s %s\n", cartas[i], paus);
             }
-            else if(trocarNaipe(cartas, hand_size, cartas[i].valor, cartas[i].naipe) == naipe_copas){
+            else if(trocarNaipe(cartas, hand_size, carta_valor, carta_naipe) == naipe_copas){
               strcat(table, copas);
               printf("DISCARD %s %s\n", cartas[i], copas);
             }
-            else if(trocarNaipe(cartas, hand_size, cartas[i].valor, cartas[i].naipe) == naipe_ouros){
+            else if(trocarNaipe(cartas, hand_size, carta_valor, carta_naipe) == naipe_ouros){
               strcat(table, ouros);
               printf("DISCARD %s %s\n", cartas[i], ouros);
             }
           }
           else{
-            strcpy(table, cartas[i].carta);
-            printf("DISCARD %s\n", cartas[i].carta);
+            strcpy(table, cartas[i]);
+            printf("DISCARD %s\n", cartas[i]);
           }
 
           for(int j = i; j < hand_size-1; j++){
-            strcpy(cartas[j], cartas[j+1].carta);
+            strcpy(cartas[j], cartas[j+1]);
           }
           hand_size--;
         }
